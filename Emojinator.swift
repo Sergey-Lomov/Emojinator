a254a799fd2232ab0b3b237e0e387a803ee21839
serhii.lomov <serhii.lomov23@gmail.com>
2021-04-12 20:33:31 +0300

    Initial commit

---
+++ Emojinator.swift
@@ -0,0 +1,67 @@
+//
+//  Emojinator.swift
+//  Prophecy
+//
+//  Created by Serhii.Lomov on 12.04.2021.
+//
+
+import Foundation
+
+final class Emojinator {
+    
+    private static var dictionary = [
+        "devil" : "👹",
+        "alien" : "👽",
+        "clown" : "🤡",
+        "robot" : "🤖",
+        "death" : "💀",
+        "ghost" : "👻",
+        "cat" : "🐈",
+        "dog" : "🐕",
+        "money" : "💰",
+        "detective" : "🕵️‍♂️",
+        "kiss" : "💋",
+        "love" : "❤️",
+        "see" : "👁️",
+        "punch" : "👊",
+        "hear" : "👂",
+        "write" : "✍️",
+        "leak" : "👅",
+        "burn" : "🔥",
+        "freez" : "❄️",
+        "mind" : "🧠",
+        "blood" : "🩸",
+        "tooth" : "🦷",
+        "family" : "👨‍👩‍👧",
+        "ambulance" : "🚑",
+        "sleep" : "🛌",
+        "clock1" : "🕐",
+        "clock2" : "🕓",
+        "clock3" : "🕗",
+        "clock4" : "🕙",
+        "moon1" : "🌑",
+        "moon2" : "🌗",
+        "moon3" : "🌕",
+        "moon4" : "🌓",
+        "weather1" : "☁️",
+        "weather2" : "🌧️",
+        "weather3" : "🌩️",
+        "weather4" : "☀️",
+        "camera" : "📹",
+        "scissros" : "✂️",
+        "miscroscope" : "🔬",
+        "radiation" : "☢️",
+        "biohazard" : "☣️",
+        "ortodox" : "☦️",
+        "latin" : "✝️",
+        "menorach" : "🕎",
+        "sixStart" : "✡️",
+        "islam" : "☪️",
+    ]
+    
+    func emojinate(string: String) -> String {
+        let words = string.split(separator: " ")
+        let emoji = words.compactMap{ Self.dictionary[String($0)] }
+        return emoji.joined()
+    }
+}
